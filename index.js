dde925cf6180ccaec06a0f1e258e12ebfb3690f9
Neelachari N H <neelachari34@gmail.com>
2022-12-15 19:52:46 +0530

    fp07_206 day-3

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let mid1=document.getElementById("Mid2")
 let Haed2js=document.getElementById("Haed2")
 
-Haed2js.addEventListener("click",=>{
+Haed2js.addEventListener("click",()=>{
     
 })
 
-mid1.addEventListener("click",=>{
+mid1.addEventListener("click",()=>{
 
 })
