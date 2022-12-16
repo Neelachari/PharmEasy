3286aa99a20fe207197f4a144088708c8b624067
Neelachari N H <neelachari34@gmail.com>
2022-12-16 19:49:59 +0530

    fp07_206 day-4

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let mid1=document.getElementById("Mid2")
 let Haed2js=document.getElementById("Haed2")
 
+
 Haed2js.addEventListener("click",()=>{
-    
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(Mid2) {
+  showSlides(slideIndex += Mid2.length-1);
+}   
 })
 
 mid1.addEventListener("click",()=>{
